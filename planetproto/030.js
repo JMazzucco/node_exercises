d70d2cfd9d18f205c2502a274de62116c427f636
John Mazzucco <John.Mazzucco@gmail.com>
2015-05-10 10:36:08 -0400

    Completed Property Assignment exercise

--- planetproto/030.js
+++ planetproto/030.js
@@ -23,7 +23,6 @@ claim(vehicle.motors, 4);
 // -> What is `robot.motors`?
 claim(robot.motors, 4);
 
-
 // ------------------------------------------------
 // Common JS exports for verification, don't modify
 module.exports = {
