8c132b2d352017799eb2747b0ddad07115c96f98
John Mazzucco <John.Mazzucco@gmail.com>
2015-05-10 09:50:15 -0400

    Completed Simple Objects exercise

--- learnyounode/firstAsync.js
+++ learnyounode/firstAsync.js
@@ -13,12 +13,3 @@ function countLines() {
 countLines()
 
 
-
-
-
-
-// var fs = require('fs');
-// var buf = fs.readFileSync(process.argv[2]);
-// var str = buf.toString().split('\n').length -1;
-
-// console.log(str);
